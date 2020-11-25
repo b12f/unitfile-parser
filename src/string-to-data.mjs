dcae06fdb0749404c54cf36b7ef5833e372ec7ce
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2020-11-25 16:49:13 +0100

    Fixed import

--- src/string-to-data.mjs
+++ src/string-to-data.mjs
@@ -1,5 +1,5 @@
 import stringToAst from "./string-to-ast.mjs";
-import astToData from "./astToData.mjs";
+import astToData from "./ast-to-data.mjs";
 
 export default function stringToData(data) {
   return astToData(stringToAst(data));
